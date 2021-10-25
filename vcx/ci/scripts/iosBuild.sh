a96cc938d52fa0511c8d2bbb2b603fb6e436a129
artem.ivanov <artem.ivanov@dsr-company.com>
2021-10-25 14:10:55 +0300

    Clean up pipeline and use VDR-Tools instead of libindy

--- vcx/ci/scripts/iosBuild.sh
+++ vcx/ci/scripts/iosBuild.sh
@@ -16,12 +16,8 @@ setup_env() {
 
     export INDY_BRANCH=$1
     export INDY_VERSION=$2
-    export NULL_BRANCH=$3
-    export NULL_VERSION=$4
-    export SOVTOKEN_VER=$5
-    export SOVTOKEN_ZIP=$6
-    export RUST_VERSION=$7
-    export VCX_VERSION=$8
+    export RUST_VERSION=$3
+    export VCX_VERSION=$4
 
     check_params
 
@@ -40,10 +36,9 @@ setup_env() {
 }
 
 check_params() {
-    if [ -z ${INDY_BRANCH} ] || [ -z ${INDY_VERSION} ] || [ -z ${NULL_BRANCH} ] || [ -z ${NULL_VERSION} ] \
-    || [ -z ${SOVTOKEN_VER} ] || [ -z ${SOVTOKEN_ZIP} ] || [ -z ${RUST_VERSION} ] || [ -z ${VCX_VERSION} ]; then
-        echo "missing parameters. Expected (INDY_BRANCH, INDY_VERSION, NULL_BRANCH, NULL_VERSION, SOVTOKEN_VER, SOVTOKEN_ZIP,
-        RUST_VERSION, VCX_VERSION)"
+    if [ -z ${INDY_BRANCH} ] || [ -z ${INDY_VERSION} ] \
+    || [ -z ${RUST_VERSION} ] || [ -z ${VCX_VERSION} ]; then
+        echo "missing parameters. Expected (INDY_BRANCH, INDY_VERSION, RUST_VERSION, VCX_VERSION)"
         exit 1
     fi
 }
